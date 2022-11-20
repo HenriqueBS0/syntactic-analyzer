f558465b36b3c64e732f1fbc00dab7bab4346523
Henrique Borges dos Santos <henrique.10agr@gamil.com>
2022-11-20 19:27:16 -0300

    Changed so that during validation it is possible to change the state of the semantic analysis object

--- src/SLR/Tree/Node.php
+++ src/SLR/Tree/Node.php
@@ -4,7 +4,7 @@ namespace HenriqueBS0\SyntacticAnalyzer\SLR\Tree;
 
 use HenriqueBS0\SyntacticAnalyzer\SLR\Semantic\SemanticAnalyzer;
 abstract class Node {
-    public function semanticValidation(SemanticAnalyzer $semanticAnalyzer) : void 
+    public function semanticValidation(SemanticAnalyzer &$semanticAnalyzer) : void 
     {
 
     } 
